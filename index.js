8c08898a60e297c27fd92e2a383469e128b431ed
shohagroy <pkshohag240@gmail.com>
2022-11-08 15:04:16 +0600

    post method and save database added

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 // meddleware
 app.use(cors());
 app.use(express.json());
+
+// database config
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  const serviceCollection = client.db("travel_point").collection("services");
+
+  app.post("/service", async (req, res) => {
+    const service = req.body;
+
+    const result = await serviceCollection.insertOne(service);
+    res.send(result);
+  });
+};
+
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Travel Point Service is Running!");
