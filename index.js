9024a1f6e1b4c7b91f1f62f8968a72b81ce99829
shohagroy <pkshohag240@gmail.com>
2022-11-08 13:51:49 +0600

    travel point server added

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// meddleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Travel Point Service is Running!");
+});
+
+app.listen(port, () => {
+  console.log(`Travel Poient Server is Running Port: ${port}`);
+});
